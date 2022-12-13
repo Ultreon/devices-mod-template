ca4ddf27c5a3e7cf018a8b48fde73052dbd70f37
Glitch <qboiwastaken@gmail.com>
2022-12-13 11:08:51 +0100

    Starts up now, doesn't get shown in app market.

--- src/main/java/com/example/exampleaddon/ExampleAddon.java
+++ src/main/java/com/example/exampleaddon/ExampleAddon.java
@@ -2,14 +2,18 @@ package com.example.exampleaddon;
 
 import com.example.exampleaddon.apps.ExampleApp;
 import com.ultreon.devices.api.ApplicationManager;
+import com.ultreon.devices.forge.ForgeApplicationRegistration;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(ExampleAddon.MOD_ID)
 public class ExampleAddon {
     public static final String MOD_ID = "exampleaddon";
     public ExampleAddon() {
-        ApplicationManager.registerApplication(ExampleAddon.res("example"), () -> ExampleApp::new, false);
+        MinecraftForge.EVENT_BUS.<ForgeApplicationRegistration>addListener(event -> {
+            ApplicationManager.registerApplication(ExampleAddon.res("example"), () -> ExampleApp::new, false);
+        });
     }
 
     private static ResourceLocation res(String path) {
